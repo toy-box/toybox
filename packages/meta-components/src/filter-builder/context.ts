91d75c28ef0dbcaf1830eb062937bf162e0d1e6c
Huhui <wishgood@gmail.com>
2021-06-19 01:45:31 +0800

    fix: 修复类型问题

--- packages/meta-components/src/filter-builder/context.ts
+++ packages/meta-components/src/filter-builder/context.ts
@@ -1,8 +1,10 @@
 import { createContext } from 'react'
 
 export interface IFilterBuilderContextProps {
-  value: Toybox.MetaSchema.Types.ICompareOperation[]
-  onChange: (value: Toybox.MetaSchema.Types.ICompareOperation[]) => void
+  value: Partial<Toybox.MetaSchema.Types.ICompareOperation>[]
+  onChange: (
+    value: Partial<Toybox.MetaSchema.Types.ICompareOperation>[]
+  ) => void
 }
 
 export const FilterBuilderContext = createContext<IFilterBuilderContextProps>({
