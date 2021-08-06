99f0602b945eb8e47c87889b6f3889e9e6ed23c1
Huhui <wishgood@gmail.com>
2021-08-06 11:31:17 +0800

    feat: FieldRow增加间距参数

--- packages/toybox-ui/src/components/field-row/index.tsx
+++ packages/toybox-ui/src/components/field-row/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, CSSProperties } from 'react'
+import React, { FC, CSSProperties, useMemo } from 'react'
 import classNames from 'classnames'
 
 import './styles'
@@ -7,21 +7,30 @@ export interface IFieldRowProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string
   style?: CSSProperties
   allowWrap?: boolean
+  size?: number
 }
 
 export const FieldRow: FC<IFieldRowProps> = ({
   className,
   allowWrap,
   style,
+  size = 8,
   children,
   ...otherProps
 }) => {
   const prefixCls = 'tbox-field-row'
+  const mixStyles = useMemo(
+    () => ({
+      ...style,
+      gap: `${size}px`,
+    }),
+    []
+  )
   return (
     <div
       {...otherProps}
       className={classNames(prefixCls, className, { allowWrap })}
-      style={style}
+      style={mixStyles}
     >
       {children}
     </div>
