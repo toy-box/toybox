3019767a31de2d8ecd4537a871e03f672efd5401
DivX <wishgood@gmail.com>
2023-02-20 16:01:27 +0800

    feat: config support json

--- packages/meta-schema/rollup.config.js
+++ packages/meta-schema/rollup.config.js
@@ -1,3 +1,4 @@
+import json from '@rollup/plugin-json'
 import baseConfig from '../../scripts/rollup.base.js'
 
-export default baseConfig('toybox.metaSchema', 'Toybox.MetaSchema')
+export default baseConfig('toybox.metaSchema', 'Toybox.MetaSchema', json())
